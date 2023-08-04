ae0ba19b54e1f351c19847d8b37751f529f857d6
jammmeess <james_bomediano@hotmail.com>
2023-08-04 18:48:28 +0800

    Phone Responsive

--- resources/views/education.blade.php
+++ resources/views/education.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     @include("layouts/navbar")
-    <div class="container" style="height: 80vh">
+    <div class="container d-none d-sm-block" style="height: 80vh">
         <div class="row my-5 text-dark">
             <h1>My Education</h1>
             <div class="col-lg-12 mt-5 text-dark">
@@ -21,6 +21,28 @@
                 <p> <i>2015-2016</i></p>
             </div>
             <div class="col-lg-12 mt-3 text-dark">
+                <h4><b>Mapua Institute of Technology</b></h4>
+                <p>Bachelor of Science in Hotel and Restaurant Management</p>
+                <p> <i>2011-2015</i></p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Responsive -->
+    <div class="container d-block d-sm-none" style="height: 100vh">
+        <div class="row my-5 text-dark">
+            <h1>My Education</h1>
+            <div class="col-12 mt-5 text-dark">
+                <h4><b>KodeGo Ph</b> </h4>
+                <p>Full-stack Web Developer</p>
+                <p> <i>2023</i></p>
+            </div>
+            <div class="col-12 mt-3 text-dark">
+                <h4><b>Magsaysay Center for Hospitality and Culinary Arts</b> </h4>
+                <p>Culinary Arts</p>
+                <p> <i>2015-2016</i></p>
+            </div>
+            <div class="col-12 mt-3 text-dark">
                 <h4><b>Mapua Institute of Technology</b></h4>
                 <p>Bachelor of Science in Hotel and Restaurant Management</p>
                 <p> <i>2011-2015</i></p>
