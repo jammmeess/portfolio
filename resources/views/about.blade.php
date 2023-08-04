cb3fd399e268f92a655212bd02fe4aca680b6d20
jammmeess <james_bomediano@hotmail.com>
2023-08-04 13:22:29 +0800

    Starting an About Page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<html lang="en">
+
+<head>
+    @include("layouts/head")
+    <title>About</title>
+</head>
+
+<body>
+    @include("layouts/navbar")
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                
+            </div>
+        </div>
+    </div>
+    @include('layouts/footer')
+</body>
+
+
+</html>
