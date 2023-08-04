08194570a285f3c699cc02fc3a3ad534615864b4
jammmeess <james_bomediano@hotmail.com>
2023-08-04 18:48:11 +0800

    Phone Responsive

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     @include("layouts/navbar")
-    <div class="container" style="height: 80vh">
+    <div class="container d-none d-sm-block" style="height: 80vh">
         <div class="row my-5 text-dark">
             <h1>Contact Me</h1>
             <div class="col-lg-12 mt-5 text-dark">
@@ -23,6 +23,30 @@
                 <p>Outlook Email Address: james_bomediano@hotmail.com</p>
             </div>
             <div class=" col-lg-12 mt-3 text-dark">
+                <h4><b>Contact Number</b> </h4>
+                <p>Contact Number: +63-9659109471</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Responsive -->
+
+    <div class="container d-block d-sm-none" style="height: 90vh">
+        <div class="row my-5 text-dark">
+            <h1>Contact Me</h1>
+            <div class="col-12 mt-3 text-dark">
+                <h4><b>LinkedIn</b> </h4>
+                <p>LinkedIn Account: <a href="https://www.linkedin.com/in/kristoffer-james-bomediano-020214271/">Kristoffer James Bomediano</a></p>
+            </div>
+            <div class=" col-12 mt-3 text-dark">
+                <h4><b>GitHub</b> </h4>
+                <p>GitHub Account: <a href="https://github.com/jammmeess">Kristoffer Bomediano | jammmeess</a></p>
+            </div>
+            <div class=" col-12 mt-3 text-dark">
+                <h4><b>Email</b> </h4>
+                <p>Outlook Email Address: james_bomediano@hotmail.com</p>
+            </div>
+            <div class=" col-12 mt-3 text-dark">
                 <h4><b>Contact Number</b> </h4>
                 <p>Contact Number: +63-9659109471</p>
             </div>
