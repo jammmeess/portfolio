450305d5940c93d0497a5494611f40e43a54024b
jammmeess <james_bomediano@hotmail.com>
2023-08-04 14:59:29 +0800

    Finished Work Experience page

---
+++ resources/views/experience.blade.php
@@ -0,0 +1,29 @@
+<html lang="en">
+
+<head>
+    @include("layouts/head")
+    <title>Work Experience</title>
+</head>
+
+<body>
+    @include("layouts/navbar")
+    <div class="container">
+        <div class="row my-5 text-dark">
+            <h1>My Work Experience</h1>
+            <div class="col-lg-12 mt-5 text-dark">
+                <h4><b>Costa Cruises</b> </h4>
+                <p>Demi Chef de Partie</p>
+                <p> <i>March 2018- March 2023</i></p>
+            </div>
+            <div class="col-lg-12 mt-3 text-dark">
+                <h4><b>Ibiza Beach Club, BGC</b> </h4>
+                <p>Commis Chef</p>
+                <p> <i>Feb 2017- Oct 2017</i></p>
+            </div>
+        </div>
+    </div>
+    @include('layouts/footer')
+</body>
+
+
+</html>
