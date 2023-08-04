7e9f95e08b08c67650993a8b06143b18867f4b66
jammmeess <james_bomediano@hotmail.com>
2023-08-04 15:54:42 +0800

    Added checkout my projects button

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     @include("layouts/navbar")
-    <div class="container">
+    <div class="container" style="height: 80vh">
         <div class="row mt-5">
             <div class="col-lg-6 mt-5">
                 <img src="/img/about/profile.jpg" alt="A photo of me" class="img-fluid" id="profile">
@@ -18,7 +18,8 @@
                 <p> I am a competent web developer proficient in a variety of technologies including <b>HTML, CSS, Bootstrap 5, JavaScript, Laravel, and MySQL. </b> </p>
                 <p>I am able to self-manage on independent projects and work effectively in a team.</p>
                 <p>My dedication to continuous learning allows me to stay current with industry trends and implement innovative solutions in my work. </p>
-                <a href="#"><button class="button mt-5">Contact Me</button></a>
+                <a href="/contact"><button class="button mt-5">Contact Me</button></a>
+                <a href="/projects"><button class="button ms-3 mt-5">Check out my projects</button></a>
             </div>
         </div>
     </div>
