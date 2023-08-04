397f17500f8f52681e5ca9ba3cc84915178c3e46
jammmeess <james_bomediano@hotmail.com>
2023-08-04 13:20:25 +0800

    Added a Navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,25 +1,24 @@
 @include('layouts/app')
 <nav class="navbar navbar-expand-lg navbar-custom">
   <div class="container-fluid" #id="navbar">
-    <a class="navbar-brand mx-4" href="/userhome"><img src="/img/logo/logo_white_main.png" style="width: 100px" /></a>
+    <a class="navbar-brand mx-4 text-light" href="/">My Portfolio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <img src="/img/icons/toggle.png" style="width: 30px">
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="/">About Me</a>
+          <a class="nav-link text-light  ms-2" href="/about">About Me</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="#">Education</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="#">My Portfolio</a>
+          <a class="nav-link text-light  ms-2" href="#">Education</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  text-light ms-2" href="#">Contact Me</a>
+          <a class="nav-link text-light ms-2" href="#">Projects</a>
         </li>
-        </div>
+        <li class=" nav-item">
+          <a class="nav-link text-light " href="#">Contact Me</a>
+        </li>
     </div>
+  </div>
 </nav>
