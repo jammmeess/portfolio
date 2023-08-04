dfcc9dd4f1e6bb8b765f748f19b9b637a3d838ff
jammmeess <james_bomediano@hotmail.com>
2023-08-04 18:49:09 +0800

    Phone responsive

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,13 @@
+<div class="footer ">
+    <div class="d-none d-sm-block">
+        &copy;<span> Kristoffer James B. Bomediano | +63-9569109471</span>
+    </div>
 
-<div class="footer">
-    &copy;<span> Kristoffer James B. Bomediano | +63-9569109471</span>
-</div>
+    <!-- Responsive -->
+    
+    <div class="footer ">
+        <div class="d-block d-sm-none">
+            &copy;<span> Kristoffer James B. Bomediano</span>
+        </div>
+
+    </div>
