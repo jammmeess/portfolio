2a88e873d1cd6f98c259db781e16fe5995f52fb8
jammmeess <james_bomediano@hotmail.com>
2023-08-04 14:56:36 +0800

    Working Web Routes

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,12 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'showHome']);
 
-Route::get('/about', [UserController::class,'showAbout']);
+Route::get('/about', [UserController::class, 'showAbout']);
+
+Route::get('/education', [UserController::class, 'showEducation']);
+
+Route::get('/experience', [UserController::class, 'showExperience']);
+
+Route::get('/projects', [UserController::class, 'showProjects']);
+
+Route::get('/contact', [UserController::class, 'showContact']);
