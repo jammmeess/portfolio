ed0ebb52d157ab43dec3a723ce9fa0c4f42a4b21
jammmeess <james_bomediano@hotmail.com>
2023-08-04 14:55:55 +0800

    Updated UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,25 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+    public function showContact()
+    {
+        return view('contact');
+    }
+
+    public function showProjects()
+    {
+        return view('projects');
+    }
+
+    public function showExperience()
+    {
+        return view('experience');
+    }
+
+    public function showEducation(){
+        return view('education');
+    }
+
     public function showAbout(){
         return view('about');
     }
