e302e107ecf599d54ceaa6038cca8bf66149d8f2
jammmeess <james_bomediano@hotmail.com>
2023-08-15 14:13:13 +0800

    Minor Changes on Nav Label

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,8 @@
 @include('layouts/app')
-<nav class="navbar navbar-expand-lg navbar-custom">
+<nav class="navbar navbar-expand-lg navbar-custom text-center">
   <div class="container-fluid" #id="navbar">
     <a class="navbar-brand mx-4 text-light" href="/">My Portfolio</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <img src="/img/icons/toggle.png" style="width: 30px">
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -11,13 +11,13 @@
           <a class="nav-link text-light  ms-2" href="/about">About Me</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light  ms-2" href="/education">Education</a>
+          <a class="nav-link text-light  ms-2" href="/education">My Education</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light  ms-2" href="/experience">Experience</a>
+          <a class="nav-link text-light  ms-2" href="/experience">My Experience</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light ms-2" href="/projects">Projects</a>
+          <a class="nav-link text-light ms-2" href="/projects">My Projects</a>
         </li>
         <li class=" nav-item">
           <a class="nav-link text-light " href="/contact">Contact Me</a>
